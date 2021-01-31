f43cccce7d5dcff1b156f59dcb697d857f85385c
jfjhh <alex.striff1@gmail.com>
2021-01-30 17:30:14 -0800

    Operator projections and cosines; changing bases

--- notebooks/TransverseIsingModels.jl
+++ notebooks/TransverseIsingModels.jl
@@ -6,11 +6,13 @@ export TransverseIsingModel, ThermalOhmicBath, jumpoperators
 
 
 # Systems
-
+#
+# TODO: System functions like `hamiltonian`, `sx`, etc. are not type-stable. A
+# solution would be to have the number of spins as a type parameter and make
+# `tpow` type-stable.
 abstract type System end
 hamiltonian(s::System) = undef
 basis(s::System) = undef
-eigen(s::System) = eigenstates(dense(hamiltonian(s)))
 
 
 # Eigenbases
@@ -21,20 +23,35 @@ struct EigenBasis <: Basis
     EigenBasis(system) = new([prod(basis(system).shape)], system)
 end
 
-function eigenmatrices(s)
-    eb = EigenBasis(s)
+eigenbasis(s) = EigenBasis(s)
+
+Base.:(==)(b1::EigenBasis, b2::EigenBasis) = (b1.shape == b2.shape) && (b1.system == b2.system)
+
+basiseigen(s) = basiseigen(s, eigenbasis(s))
+
+function basiseigen(s, b)
     H = hamiltonian(s)
     bl, br = H.basis_l, H.basis_r
     vals, vecs = eigen(dense(H).data)
-    P = Operator(eb, br, inv(vecs))
-    Pinv = Operator(bl, eb, vecs)
-    P, Pinv, vals
+    _basiseigen(vals, vecs, bl, br, b)
+end
+
+function _basiseigen(vals, vecs, bl, br, b::Basis)
+    P, Pinv = identityoperator(b, br), identityoperator(bl, b)
+    kets = [Ket(b, vecs[:, i]) for i in eachindex(vals)]
+    vals, kets, P, Pinv
+end
+
+function _basiseigen(vals, vecs, bl, br, b::EigenBasis)
+    P, Pinv = Operator(b, br, inv(vecs)), Operator(bl, b, vecs)
+    kets = [basisstate(b, i, sparse=true) for i in eachindex(vals)]
+    vals, kets, P, Pinv
 end
 
 
 # Utilities
 
-sparsify(xs, atol=sqrt(eps(1.0))) = sparse([abs(x) > atol ? x : 0.0 for x in xs])
+sparsify(A, atol=sqrt(eps(1.0))) = sparse([abs(x) > atol ? x : zero(x) for x in A])
 
 function sparsify(op::Operator)
     bl, br = op.basis_l, op.basis_r
@@ -88,13 +105,16 @@ const σp, σm = sigmap(sb), sigmam(sb)
 "`tpow(x, n)` takes the tensor product of `x` with itself `n` times."
 tpow(x, n) = ⊗(repeat([x], n)...)
 
-struct TransverseIsingModel <: System
-    λ::Number # Currently λ_Striff. Should change to g = -λ_Striff in the future
-    N::Int
-end
+abstract type SpinSystem <: System end
 
-spinbasis(s) = tpow(sb, s.N)
-Ispin(s) = identityoperator(spinbasis(s))
+spinbasis(s::SpinSystem) = tpow(sb, s.N)
+Ispin(s::SpinSystem) = identityoperator(spinbasis(s))
+basis(s::SpinSystem) = spinbasis(s)
+
+struct TransverseIsingModel <: SpinSystem
+    N::Int
+    λ::Number # Currently λ_Striff. Should change to g = -λ_Striff in the future
+end
 
 # Define the Pauli operators at each site
 for (opi, op) in [(:sx,:σx), (:sy,:σy), (:sz,:σz), (:sp,:σp), (:sm,:σm)]
@@ -106,7 +126,6 @@ end
 
 Hspin(s) = -sum(-s.λ*sz(s, i) + sx(s, i)*sx(s, i+1) for i in 1:s.N)
 
-basis(s::TransverseIsingModel) = spinbasis(s)
 hamiltonian(s::TransverseIsingModel) = Hspin(s)
 
 k(s, m) = 2π*(m-1)/s.N - π*(s.N - (s.N%2))/s.N # for m in 1:s.N
@@ -213,7 +232,7 @@ function spectraldensity(bath::ThermalOhmicBath, ω)
     (2*bath.rate / π) * ω / (1 + (ω / bath.cutoff)^2)
 end
 
-# TODO: Verify constants, polariztion, etc. for magnetic 3D spin interaction
+# TODO: Verify constants, polarization, etc. for magnetic 3D spin interaction
 # TODO: Check limit ω->0 case
 function γ(bath::ThermalOhmicBath, ω)
     # g = V * (ω^2 / (2π*c)^3) * 8π/3
@@ -229,9 +248,14 @@ end
 
 # Construction of jump operators
 
-function dictby(A; isequal=isequal, keyof=(i, x) -> x, valof=(i, x) -> i)
-    i0 = firstindex(A)
-    x0 = A[i0]
+firstvalue(i, (x, y)) = x
+lastvalue(i,  (x, y)) = y
+
+# TODO: Rewrite things to use dictmap to fmap over ωs.
+dictmap(f, dict) = Dict(key => f(value) for (key, value) in dict)
+
+function dictby(A; isequal=isequal, keyof=firstvalue, valof=lastvalue)
+    i0, x0 = 1, first(A)
     k0, v0 = keyof(i0, x0), valof(i0, x0)
     dict = Dict(k0 => typeof(v0)[])
     for (i, x) in enumerate(A)
@@ -246,25 +270,15 @@ function dictby(A; isequal=isequal, keyof=(i, x) -> x, valof=(i, x) -> i)
     dict
 end
 
-function dictbysort(itr; keyof=(i, x) -> x[1], valof=(i, x) -> x[2],
+function dictbysort(itr; keyof=firstvalue, valof=lastvalue,
         isequal=isequal, by=x -> x[1], kwargs...)
     A = sort([(keyof(i, x), valof(i, x)) for (i, x) in enumerate(itr)];
         by=by, kwargs...)
-    dictby(A, isequal=isequal, keyof=(i, x) -> x[1], valof=(i, x) -> x[2])
+    dictby(A, isequal=isequal)
 end
 
-function addentry!(dict, key, value, isequal=isequal)
-    for k in keys(dict)
-        if isequal(k, key)
-            push!(dict[k], value)
-            return dict
-        end
-    end
-    dict[key] = [value]
-    dict
-end
-
-eigendict(s::System) = dictbysort(zip(eigen(s)...), isequal=isapprox)
+# `Es` are guaranteed to be sorted.
+eigendict(Es, kets) = dictby(zip(Es, kets), isequal=isapprox)
 
 function energydiffs(eigdict)
     Es = keys(eigdict)
@@ -276,21 +290,53 @@ function projectors(eigdict)
          for (energy, eigstates) in eigdict)
 end
 
-# TODO: Generator?
-function jumpoperators(system, As)
-    eigdict = eigendict(system)
-    Πs = projectors(eigdict)
-    Dict(ω => [sum(Πs[E1] * A * Πs[E2] for (E1, E2) in Ediffs)
-                     for A in As] for (ω, Ediffs) in energydiffs(eigdict))
+function changebasis(As, P, Pinv; sparse=true)
+    Bs = [P * A * Pinv for A in As]
+    sparse ? sparsify.(Bs) : Bs
 end
 
-# TODO: Combine us so that there is only one `eigen` call, and allow eigenbasis
-# calculations.
-function jumpoperators(s::TransverseIsingModel)
-    P, Pinv, Es = eigenmatrices(s)
-    As = [op(s, i) for i in 1:s.N for op in [sx, sy, sz]]
-    jumpoperators(s, As)
+# This recalculates the eigensystem, so a better way of only calculating this
+# once may be necessary to consider larger N.
+function changebasis(As, s::System; basis=eigenbasis(s), kwargs...)
+    _, _, P, Pinv = basiseigen(s, basis)
+    changebasis(As, P, Pinv; kwargs...)
 end
+
+# TODO: Double-check energies. N=8 looked like 0-ish dict keys were not joined.
+function jumpoperators(s::SpinSystem, interactions; basis=eigenbasis(s))
+    Es, kets, P, Pinv = basiseigen(s, basis)
+    eigdict = eigendict(Es, kets)
+    As = changebasis(interactions, P, Pinv)
+    Πs = projectors(eigdict)
+    Dict(ω => [sum(Πs[E1] * A * Πs[E2] for (E1, E2) in Ediffs)
+               for A in As] for (ω, Ediffs) in energydiffs(eigdict))
+end
+
+interactions(s::TransverseIsingModel) = [op(s, i) for i in 1:s.N, op in [sx, sy, sz]]
+
+jumpoperators(s::SpinSystem; kwargs...) = jumpoperators(s, interactions(s); kwargs...)
+
+function qubitjumps(s::SpinSystem; basis=eigenbasis(s))
+    Qs = [op(s, i) for i in 1:s.N, op in [sp, sm]]
+    changebasis(Qs, s, basis=basis)
+end
+
+# These can likely be made more efficient.
+opip(A, B) = tr(dagger(A) * B)
+opip(A) = tr(dagger(A) * A)
+opnorm(A) = √opip(A)
+opnormalize(A) = A / opnorm(A)
+opcos(A, B) = real(abs(opip(A, B)) / (opnorm(A) * opnorm(B)))
+
+# TODO: Handle zero jump operators.
+
+project(P, J) = J == zero(J) ? zero(eltype(J)) : opip(P / opip(P), J)
+jumpprojections(Js, Ps) = [project(P, J) for J in Js, P in Ps]
+jumpprojections(Jωs::Dict, Ps) = Dict(ω => jumpprojections(Js, Ps) for (ω, Js) in Jωs)
+
+jumpcos(P, J) = J == zero(J) ? real(one(eltype(J))) : opcos(P, J)
+jumpcosines(Js, Ps) = [jumpcos(P, J) for J in Js, P in Ps]
+jumpcosines(Jωs::Dict, Ps) = Dict(ω => jumpcosines(Js, Ps) for (ω, Js) in Jωs)
 
 end
 
